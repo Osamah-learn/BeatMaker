94958a657899c64af27c9233246c4d154e52e9fb
Osamah Amer <osamah.amer@learnit.fi>
2021-08-22 00:03:00 +0300

    Sound playing is ready to work

--- app.js
+++ app.js
@@ -7,19 +7,44 @@ class DrumKit {
         this.snareAudio = document.querySelector('.snare-sound')
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
-        this.bpm = 40;
+        this.bpm = 200;
     }
-// we check that the index of 0 dosnt go over 8 elements
+    // we check that the index of 0 dosnt go over 8 elements
     repeat() {
+        // reset index.0 after 7 element
         let step = this.index % 8;
-        const activeBars = document.querySelectorAll(`.b${step}`)
+        // we catch b class element to loop throw
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        // we catch each bar in active bars and ease it in-out by keyFrame
+        activeBars.forEach((bar) => {
+            bar.style.animation = "playTrack 0.3s alternate ease-in-out 2";
+            // Check if pads are active
+            if (bar.classList.contains('active')) {
+                // check if each sound is clicked
+                if (bar.classList.contains('kick-pad')) {
+                    this.kickAudio.play();
+                    //reset the time 
+                    this.kickAudio.currentTime=0;
+                }
+                if (bar.classList.contains('snare-pad')) {
+                    this.snareAudio.play();
+                    this.snareAudio.currentTime=0;
+                }
+                if (bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.play();
+                    this.hihatAudio.currentTime=0;
+                }
+            }
+        })
         console.log(activeBars);
         this.index++;
-
-
-
     }
-// start the BeatMaker
+
+
+
+    // Methods
+
+    // start the BeatMaker
     start() {
         // The speed of steps 
         const speed = this.bpm * 1000 / 60
@@ -27,21 +52,31 @@ class DrumKit {
             this.repeat();
         }, speed)
     }
-// Change the pads class to active color 
-    activePad(){
-       this.classList.toggle('active')
+    // Change the pads class to active color 
+    activePad() {
+        this.classList.toggle('active')
     }
 }
 
 
+
+
+
+
+
+
+
 const drumKit = new DrumKit();
 // Run start Method 
-drumKit.play.addEventListener('click',function(){
+drumKit.play.addEventListener('click', function () {
     drumKit.start();
 })
 
 // loop throw pads to change css backgrounds
 
 drumKit.pads.forEach(pad => {
-    pad.addEventListener('click',drumKit.activePad)
+    pad.addEventListener('click', drumKit.activePad)
+    pad.addEventListener("animationend", function () {
+        pad.style.animation = '';
+    })
 })
